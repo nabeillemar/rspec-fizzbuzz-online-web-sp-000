33d4a0f546653a4f75328d69dd2f81156f29ee3b
Nabeil Lemar <nabeillemar@gmail.com>
2020-06-30 22:34:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,9 @@ def fizzbuzz(int)
     return "Fizz"
     elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 || int % 5 == 0
+  return "nil"
+  elsif int % 3 == 0 || int % 5 == 0 
   return "FizzBuzz"
   end
 end
